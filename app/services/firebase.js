aee1c0c13a134685cfbb715b4aa3da0136ad0d06
Adam Kvant <kvant003@umn.edu>
2024-11-11 17:12:55 -0600

    forebase

--- app/services/firebase.js
+++ app/services/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDpITRVoa5AOMiY4Yi-a7Ba8QveOs_0gek',
-  authDomain: 'f2024-ongoing-example.firebaseapp.com',
-  projectId: 'f2024-ongoing-example',
-  storageBucket: 'f2024-ongoing-example.firebasestorage.app',
-  messagingSenderId: '595362775097',
-  appId: '1:595362775097:web:15cdb97ef93c4f42b58de8',
+  apiKey: "AIzaSyBf7lvGyCrucV-syrmHc_LBLdQxyFJCvkQ",
+  authDomain: "inclass-5117.firebaseapp.com",
+  projectId: "inclass-5117",
+  storageBucket: "inclass-5117.firebasestorage.app",
+  messagingSenderId: "554278415504",
+  appId: "1:554278415504:web:ebfd2ec4addf1d810bbc15",
+  measurementId: "G-RTQKZLK88K"
 };
 
 export default class FirebaseService extends Service {
